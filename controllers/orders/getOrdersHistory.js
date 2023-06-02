6d7f5c016c61ae48a5a57014343d62537cf46752
KaminskySergey <kaminsky_s_s@ukr.net>
2023-06-02 22:17:17 +0300

    ф

--- controllers/orders/getOrdersHistory.js
+++ controllers/orders/getOrdersHistory.js
@@ -1,12 +1,12 @@
 // const { Order } = require("../../models/ordersModels");
 // const { User } = require("../../models/ordersModels");
 
-const getOrdersHistory = (req, res, next) => {
-    const { email, phone } = req.body;
-    console.log(email, phone, 'qqqqqqqqqqqqqwwwwwwwwwwww')
-}
+// const getOrdersHistory = (req, res, next) => {
+//     const { email, phone } = req.body;
+//     console.log(email, phone, 'qqqqqqqqqqqqqwwwwwwwwwwww')
+// }
 
 
 
 
-module.exports = getOrdersHistory
+// module.exports = getOrdersHistory
--- controllers/orders/index.js
+++ controllers/orders/index.js
@@ -1,11 +1,11 @@
 
 const getOrdersPost = require('./getOrdersPost')
 const getOrdersClear = require('./getOrdersClear')
-const getOrdersHistory = require('./getOrdersHistory')
+// const getOrdersHistory = require('./getOrdersHistory')
 
 
 module.exports = {
     getOrdersPost,
     getOrdersClear,
-    getOrdersHistory,
+    // getOrdersHistory,
 }
